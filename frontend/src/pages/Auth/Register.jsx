bb874a766e7cc11d5e3ec1aa7ed4aa40c4e554da
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-05-19 15:08:15 -0300

    :construction: Authentication service

--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -5,19 +5,53 @@ import { Link } from 'react-router-dom';
 import './Auth.css';
 
 export function Register() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
   function handleSubmit(e) {
     e.preventDefault();
+
+    const user = {
+      name,
+      email,
+      password,
+      confirmPassword
+    };
+
+    console.log(user);
   }
 
   return (
-    <div>
+    <div id="register">
       <h2>ReactGram</h2>
       <p className="subtitle">Cadastra-se para ver as fotos dos seus amigos.</p>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Nome" />
-        <input type="email" placeholder="E-mail" />
-        <input type="password" placeholder="Senha" />
-        <input type="password" placeholder="Confirme a senha" />
+        <input
+          type="text"
+          placeholder="Nome"
+          onChange={e => setName(e.target.value)}
+          value={name}
+        />
+        <input
+          type="email"
+          placeholder="E-mail"
+          onChange={e => setEmail(e.target.value)}
+          value={email}
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          onChange={e => setPassword(e.target.value)}
+          value={password}
+        />
+        <input
+          type="password"
+          placeholder="Confirme a senha"
+          onChange={e => setConfirmPassword(e.target.value)}
+          value={confirmPassword}
+        />
         <input type="submit" value="Cadastrar" />
       </form>
       <p>
---
+++ frontend/src/services/authService.js
@@ -0,0 +1,22 @@
+import { api, requestConfig } from '../utils/config';
+
+// Register an user
+async function register(data) {
+  const config = requestConfig('POST', data);
+
+  try {
+    const res = await fetch(`${api}/users/register`, config)
+      .then(res => res.json())
+      .catch(err => err);
+
+    if (res) {
+      localStorage.setItem('user', JSON.stringify(res));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export const authService = {
+  register
+};
