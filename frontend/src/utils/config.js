8027b0835cf3ff75c74ead6b1146ba8632bda086
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-06-07 18:31:18 -0300

    :wrench: Add API URL

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,5 +1,5 @@
-export const api = 'http://localhost:5000/api';
-export const uploads = 'http://localhost:5000/uploads';
+export const api = process.env.API_URL;
+export const uploads = `${api}/uploads`;
 
 export function requestConfig(method, data, token = null, image = null) {
   let config;
