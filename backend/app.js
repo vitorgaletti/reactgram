85e1ae471b22a89b4f5aafcd2ae0537071144a94
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-06-07 21:01:08 -0300

    :lock: Fix cors

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(cors({ credentials: true, origin: process.env.WEB_HOST }));
+app.use(cors());
 
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
