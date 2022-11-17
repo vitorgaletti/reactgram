bd76c6a55d42281902eb4742b27fa390a21e1709
Vitor <vitor.fgaletti@gmail.com>
2022-11-17 10:44:53 -0300

    🔒️ Update cors

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(cors({ origin: web, optionsSuccessStatus: 200 }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', web);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  app.use(cors());
+  next();
+});
 
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
