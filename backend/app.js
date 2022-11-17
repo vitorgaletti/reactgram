e97766342b23b021ec8eb79563a5a8ce88d3d323
Vitor <vitor.fgaletti@gmail.com>
2022-11-17 10:48:24 -0300

    🔒️ Update cors

--- backend/app.js
+++ backend/app.js
@@ -5,14 +5,16 @@ const path = require('path');
 const cors = require('cors');
 
 const port = process.env.PORT;
-const web = process.env.WEB_HOST;
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', web);
+  res.header(
+    'Access-Control-Allow-Origin',
+    'https://reactgram-vitor.vercel.app'
+  );
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   app.use(cors());
   next();
